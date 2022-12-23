4c1170f1c9c9a62e820c28e5a86787ce323f3198
Sumeyye <sumeyyedagdevirozmen@gmail.com>
2022-12-23 12:08:09 +0000

    23/12/22

---
+++ src/TreeMap3/Testclass.java
@@ -0,0 +1,10 @@
+package TreeMap3;
+
+public class Testclass {
+    public static void main(String[] args) {
+        System.out.println("heloo");
+        System.out.println("test");
+         System.out.println("test");
+         System.out.println("test2");
+    }
+}
